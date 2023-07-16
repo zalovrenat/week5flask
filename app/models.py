0e0777f0dfd1a9639bd050fbd0b43562917c54a5
Renat Zalov <renat.a.zalov@gmail.com>
2023-07-16 11:15:30 -0500

    Pokemon with working Pokedex, My Pokemon, and Pokemon Release functionality

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash
 
 db = SQLAlchemy()
 
@@ -15,10 +16,72 @@ class User(db.Model, UserMixin):
     def __init__(self,username,email,password):
         self.username = username
         self.email = email
-        self.password = password
+        self.password = generate_password_hash(password)
     
     def get_id(self):
         try:
             return str(self.user_id)
         except AttributeError:
             raise NotImplementedError("No `id` attribute - override `get_id`") from None
+
+class Pokedex(db.Model):
+    pokedex_id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.user_id'),nullable=False,unique=True)
+
+    def __init__(self, user_id):
+        self.user_id = user_id
+
+class PokedexPokemon(db.Model):
+    pokedex_pokemon_id = db.Column(db.Integer,primary_key=True)
+    pokedex_id = db.Column(db.Integer,db.ForeignKey('pokedex.pokedex_id'),nullable=False)
+    pokemon_id = db.Column(db.Integer,db.ForeignKey('pokemon.pokemon_id'),nullable=False)
+
+    def __init__(self, pokedex_id, pokemon_id):
+        self.pokedex_id = pokedex_id
+        self.pokemon_id = pokemon_id
+
+class Pool(db.Model):
+    pool_id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.user_id'),nullable=False,unique=True)
+
+    def __init__(self, user_id):
+        self.user_id = user_id
+
+class PoolPokemon(db.Model):
+    pool_pokemon_id = db.Column(db.Integer,primary_key=True)
+    pool_id = db.Column(db.Integer,db.ForeignKey('pool.pool_id'),nullable=False)
+    pokemon_id = db.Column(db.Integer,db.ForeignKey('pokemon.pokemon_id'),nullable=False)
+
+    def __init__(self, pool_id, pokemon_id):
+        self.pool_id = pool_id
+        self.pokemon_id = pokemon_id
+
+class Pokemon(db.Model):
+    pokemon_id = db.Column(db.Integer,primary_key=True)
+    pokemon_name = db.Column(db.String(25),nullable=False)
+    img_url = db.Column(db.String,nullable=False)
+    ability1 = db.Column(db.String(50),default='')
+    ability2 = db.Column(db.String(50),default='')
+    base_xp = db.Column(db.Integer,nullable=False,default=0)
+    hp_base_stat = db.Column(db.Integer,nullable=False,default=0)
+    attack_base_stat = db.Column(db.Integer,nullable=False,default=0)
+    defense_base_stat = db.Column(db.Integer,nullable=False,default=0)
+
+    def __init__(self,pokemon_id,pokemon_name,img_url,ability1,ability2,base_xp,hp_base_stat,attack_base_stat,defense_base_stat):
+        self.pokemon_id = pokemon_id
+        self.pokemon_name = pokemon_name
+        self.img_url = img_url
+        self.ability1 = ability1
+        self.ability2 = ability2
+        self.base_xp = base_xp
+        self.hp_base_stat = hp_base_stat
+        self.attack_base_stat = attack_base_stat
+        self.defense_base_stat = defense_base_stat
+
+class Team(db.Model):
+    team_id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.user_id'),nullable=False,unique=True)
+    pokemon_id = db.Column(db.Integer,db.ForeignKey('pokemon.pokemon_id'),nullable=False)
+
+    def __init__(self, user_id):
+        self.user_id = user_id
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
 from app import app
 from .forms import PokemonSearch, LoginForm, SignUpForm
-from .models import User, db
+from .models import User, db, Pokedex, PokedexPokemon, Pool, PoolPokemon, Pokemon
 from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.security import check_password_hash
 import requests as r
 
 @app.route('/')
@@ -37,13 +38,13 @@ def favorite5Page():
     return render_template('favorite5.html', favorite_pokemons = favorite_pokemons)
 
 def get_pokemon_info(pokemon_name):
-    message = None
     res = r.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon_name}')
     if res.ok:
         data = res.json()
         pokemon_data = dict()
-        pokemon_data['abilities'] = dict()
+        pokemon_data['pokemon #'] = data['id']
         pokemon_data['name'] = data['forms'][0]['name'].title()
+        pokemon_data['abilities'] = dict()
         i = 1
         for ability in data['abilities']:
             pokemon_data['abilities']['Ability ' + str(i)] = ability['ability']['name']
@@ -55,25 +56,33 @@ def get_pokemon_info(pokemon_name):
         pokemon_data['hp base stat'] = data['stats'][0]['base_stat']
         pokemon_data['defense base stat'] = data['stats'][2]['base_stat']
         return pokemon_data
-    else:
-        print('That is not a valid pokemon name. Please see a list of pokemon names here:\nhttps://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number')
 
 @app.route('/pokemon', methods=['GET','POST'])
 def pokemonSearch():
+    global global_pokemon_data
     poke_data = dict()
     form = PokemonSearch()
-    message = None
-    message_link = None
     if request.method == 'POST':
         if form.validate():
             pokemon = form.pokename.data
             poke_data = get_pokemon_info(pokemon.lower())
+            print(poke_data)
+            global_pokemon_data = poke_data
+            if Pokemon.query.filter_by(pokemon_id=poke_data['pokemon #']).first():
+                pass
+            elif len(poke_data['abilities']) > 1:
+                addPokemon = Pokemon(poke_data['pokemon #'],poke_data['name'],poke_data['img'],poke_data['abilities']['Ability 1'],poke_data['abilities']['Ability 2'],poke_data['base experience'],poke_data['hp base stat'],poke_data['attack base stat'],poke_data['defense base stat'])
+                db.session.add(addPokemon)
+                db.session.commit()
+            else:
+                addPokemon = Pokemon(poke_data['pokemon #'],poke_data['name'],poke_data['img'],poke_data['abilities']['Ability 1'],None,poke_data['base experience'],poke_data['hp base stat'],poke_data['attack base stat'],poke_data['defense base stat'])
+                db.session.add(addPokemon)
+                db.session.commit()
 
             if not poke_data:
-                message = 'That is not a valid pokemon name. Please see a list of pokemon names here:'
-                message_link = 'https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number'
+                flash('That is not a valid pokemon name. Please see a list of valid pokemon names ','warning')
     
-    return render_template('pokemon.html', form=form, pokemon_data=poke_data, message=message, message_link=message_link)
+    return render_template('pokemonsearch.html', form=form, pokemon_data=poke_data)
 
 @app.route('/login', methods=['GET','POST'])
 def loginPage():
@@ -88,17 +97,24 @@ def loginPage():
             user = User.query.filter_by(username=username).first()
 
             if user:
-                if user.password == password:
+                if check_password_hash(user.password, password):
                     login_user(user)
+                    flash('Successfuly logged in.', 'success')
                     return redirect(url_for('homePage'))
                 else:
-                    message = 'Incorrect password.'
-                    print('Incorrect password.')
+                    flash('Incorrect password.', 'danger')
             else:
-                message = 'The username does not exist'
-                print('The username does not exist')
+                flash('The username does not exist.', 'danger')
+        else:
+            flash('An error has ocurred. PLease submit a valid form.', 'danger')
 
     return render_template('login.html', form=form, message=message)
+
+def username_in_db(username):
+    return User.query.filter_by(username=username).first()
+
+def email_in_db(email):
+    return User.query.filter_by(email=email).first()
 
 @app.route('/signup', methods=['GET','POST'])
 def signUpPage():
@@ -108,18 +124,123 @@ def signUpPage():
             username = form.username.data
             email = form.email.data
             password = form.password.data
+
+            if username_in_db(username):
+                flash('The username already exists. Please enter another username.', 'danger')
+                return redirect(url_for('signUpPage'))
+
+            elif email_in_db(email):
+                flash('The email is already in use. Please enter another email.', 'danger')
+                return redirect(url_for('signUpPage'))
             
-            # add user to database
-            user = User(username,email,password)
+            else:
+                # add user to database
+                user = User(username,email,password)
 
-            db.session.add(user)
-            db.session.commit()
+                db.session.add(user)
+                db.session.commit()
 
-            return redirect(url_for('loginPage'))
+                user = User.query.filter_by(username=username).first()
+                user_id = user.user_id
+                pokedex = Pokedex(user_id)
+
+                db.session.add(pokedex)
+                db.session.commit()
+
+                pool = Pool(user_id)
+
+                db.session.add(pool)
+                db.session.commit()
+
+                flash('Successfully created an account.', 'success')
+
+                return redirect(url_for('loginPage'))
+
+        else:
+            flash('Passwords do not match. Please try again.', 'danger')
     
     return render_template('signup.html', form=form)
 
 @app.route('/logout')
+@login_required
 def logout():
     logout_user()
+    flash('Successfully logged out.', 'success')
     return redirect(url_for('loginPage'))
+
+@app.route('/catch', methods=['GET','POST'])
+@login_required
+def catchPokemon():
+    current_pool = Pool.query.filter_by(user_id=current_user.user_id).first().pool_id
+    pokemon_id = global_pokemon_data['pokemon #']
+    pool_pokemon = PoolPokemon(current_pool,pokemon_id)
+    db.session.add(pool_pokemon)
+    db.session.commit()
+    flash('Pokemon successfully added to your collection.', 'success')
+
+    current_pokedex = Pokedex.query.filter_by(user_id=current_user.user_id).first().pokedex_id
+    dexPokes = PokedexPokemon.query.filter_by(pokedex_id=current_pokedex).all()
+    pokedexPokemon = dict()
+    for poke in dexPokes:
+        pokedexPokemon[poke.pokemon_id] = pokemon_id
+    if not PokedexPokemon.query.filter_by(pokedex_id=current_pokedex).all():
+        addPokedexPokemon = PokedexPokemon(current_pokedex, pokemon_id)
+        db.session.add(addPokedexPokemon)
+        db.session.commit()
+    elif pokemon_id in pokedexPokemon:
+        pass
+    else:
+        addPokedexPokemon = PokedexPokemon(current_pokedex, pokemon_id)
+        db.session.add(addPokedexPokemon)
+        db.session.commit()
+        flash('Pokemon successfully added to your Pokedex.', 'success')
+    
+    return redirect(url_for('pokemonSearch'))
+
+@app.route('/pokedex')
+@login_required
+def pokedex():
+    current_pokedex = Pokedex.query.filter_by(user_id=current_user.user_id).first().pokedex_id
+    pokemon_list = PokedexPokemon.query.filter_by(pokedex_id=current_pokedex).all()
+    pokemon_id_list = []
+    for pokemon in pokemon_list:
+        pokemon_id_list.append(pokemon.pokemon_id)
+    pokemon_id_list.sort()
+    all_pokemon = []
+    for poke_id in pokemon_id_list:
+        all_pokemon.append(Pokemon.query.filter_by(pokemon_id=poke_id).first())
+
+    return render_template('pokedex.html', all_pokemon=all_pokemon)
+
+@app.route('/mypokemon')
+@login_required
+def myPokemon():
+    current_pool = Pool.query.filter_by(user_id=current_user.user_id).first().pool_id
+    pokemon_list = PoolPokemon.query.filter_by(pool_id=current_pool).all()
+    all_pokemon = dict()
+    for pokemon in pokemon_list:
+        all_pokemon[pokemon.pool_pokemon_id] = Pokemon.query.filter_by(pokemon_id=pokemon.pokemon_id).first()
+    # pokemon_id_list.sort()
+    # all_pokemon = dict()
+    # for poke_id in pokemon_id_list:
+    #     all_pokemon[] = Pokemon.query.filter_by(pokemon_id=poke_id).first()
+    return render_template('mypokemon.html', all_pokemon=all_pokemon)
+
+@app.route('/mypokemon/<pool_pokemon_id>')
+@login_required
+def singlePokemonPage(pool_pokemon_id):
+    pokemon = PoolPokemon.query.get(pool_pokemon_id)
+    if pokemon:
+        poke = Pokemon.query.get(pokemon.pokemon_id)
+        return render_template('singlepokemon.html', poke=poke, pokemon=pokemon)
+    else:
+        return redirect(url_for('myPokemon'))
+
+@app.route('/mypokemon/release/<pool_pokemon_id>')
+@login_required
+def releasePokemon(pool_pokemon_id):
+    pokemon = PoolPokemon.query.get(pool_pokemon_id)
+    db.session.delete(pokemon)
+    db.session.commit()
+    flash('Pokemon successfully released.', 'success')
+    return redirect(url_for('myPokemon'))
